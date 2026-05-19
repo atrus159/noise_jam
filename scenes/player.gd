eb31740a348fb09a15d2095a1ec4e23b0e999f31
Darius Rudominer <darius.is.ru@gmail.com>
2026-05-19 19:27:40 -0400

    added human move

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 var cell_size;
 var tileMap;
+var turn;
+var timer;
 
 func _ready():
 	tileMap = get_parent().get_node("TileMapLayer")
 	cell_size = tileMap.tile_set.tile_size.x;
+	turn = true
+	timer = 1
+	randomize()
 
 func _check_tile_map_pos(x: float, y: float) -> bool:
 	var cell = tileMap.local_to_map(tileMap.to_local(Vector2(x,y)))
@@ -25,13 +30,37 @@ func _angel_move(dir: float) -> bool:
 	position.x = curX
 	position.y = curY
 	return true
+	
+func _human_move():
+	var choises = [0,90,180,270]
+	var dir = choises[randi() % 4]
+	var xMod = cell_size * cos(deg_to_rad(dir))
+	var yMod = cell_size * sin(deg_to_rad(dir))
+	while _check_tile_map_pos(position.x + xMod, position.y + yMod):
+		dir = choises[randi() % 4]
+		xMod = cell_size * cos(deg_to_rad(dir))
+		yMod = cell_size * sin(deg_to_rad(dir))
+	position.x += xMod
+	position.y += yMod
 
 func _process(delta):
-	if Input.is_action_just_pressed("move_left"):
-		_angel_move(180)
-	if Input.is_action_just_pressed("move_right"):
-		_angel_move(0)
-	if Input.is_action_just_pressed("move_up"):
-		_angel_move(270)
-	if Input.is_action_just_pressed("move_down"):
-		_angel_move(90)
+	if(turn):
+		var succeded = false
+		if Input.is_action_just_pressed("move_left"):
+			succeded = _angel_move(180)
+		if Input.is_action_just_pressed("move_right"):
+			succeded = _angel_move(0)
+		if Input.is_action_just_pressed("move_up"):
+			succeded = _angel_move(270)
+		if Input.is_action_just_pressed("move_down"):
+			succeded = _angel_move(90)
+		if succeded:
+			turn = false
+			timer = 1
+	else:
+		timer -= delta
+		if timer <= 0:
+			_human_move()
+			turn = true
+		
+			
